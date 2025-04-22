7241ac3fde3c1b68a43b08341b0d3e2e991aa5b8
Ricardo Flores Estrella <ricardoandresf96@gmail.com>
2025-04-22 18:52:42 +0200

    Commit inicial

---
+++ src/ricardoflores/Principal.java
@@ -0,0 +1,20 @@
+package ricardoflores;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca el número sobre el que hacer los cálculos");
+		
+		int numero = sc.nextInt();
+		
+		new CalculosMatematicos().calculos(numero);
+
+	}
+
+}
